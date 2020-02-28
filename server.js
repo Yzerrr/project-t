6f22977d5947e94f90857d8bf1e75d5057039e89
Yzerrr <joeyyzer@gmail.com>
2020-02-28 11:53:48 +0100

    .

---
+++ server.js
@@ -0,0 +1,26 @@
+let data ={
+    title: "dateApp",
+    page: "about",
+    name: "joey"
+};
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+const ejs = require('ejs')
+
+app.use(express.static('public'))
+app.set("view engine","ejs")
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get("/about", (req, res) =>{
+    res.render("base/about.ejs", {data:'string'});
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.get('*', (req, res) => res.send('404'));
+ 
+
