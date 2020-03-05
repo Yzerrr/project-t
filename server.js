97f147bd77dae966f23974bee7ebc4ec143c2eef
Yzerrr <joeyyzer@gmail.com>
2020-03-05 22:29:32 +0100

    ejs

--- server.js
+++ server.js
@@ -1,27 +1,40 @@
-let data ={
-    title: "dateApp",
-    page: "about",
-    name: "joey"
-};
-
-
 const express = require('express')
 const app = express()
 const port = 3000
 const ejs = require('ejs')
 
+
+
+let data = {
+    title: "dateApp",
+    page: "about",
+    name: "test"
+};
+
+
 app.use(express.static('public'))
-app.set("view engine","ejs")
+app.set("view engine", "ejs")
+// app.set("views", "base")
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.get("/about", (req, res) =>{
-    res.render("base/about.ejs", {data:'string'});
+app.get("/about", (req, res) => {
+    res.render("base/about.ejs", {
+        data
+    });
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get("/index", (req, res) => {
+    res.render("base/index.ejs", {
+        data
+    });
+});
 
-app.get('*', (req, res) => res.send('404'));
- 
 
- 
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`listening on port ${port}`))
+
+
+app.use(function (req, res) {
+    res.send("404: Page not found", 404);
+});
