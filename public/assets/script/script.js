789d24b6c070f2c4b5723b4678c9cc75d6166c90
Yzerrr <joeyyzer@gmail.com>
2020-03-16 13:30:01 +0100

    sending data to collection

--- public/assets/script/script.js
+++ public/assets/script/script.js
@@ -1,13 +1,12 @@
 // console.log("working");
 
 let overlayBlur = document.querySelector(".overlay-blur");
+let form = document.getElementById('mForm');
 let showProfileButton = document.querySelector(".button");
 let addBlur = document.querySelector(".add-blur");
+let submitButton = document.getElementById('submit');
 
- 
-
-
-showProfileButton.addEventListener("mousedown", function () {
+/* showProfileButton.addEventListener("mousedown", function () {
     overlayBlur.style.opacity = "0.1";
 
 
@@ -18,6 +17,36 @@ showProfileButton.addEventListener("mouseup", function () {
     overlayBlur.style.opacity = "1.2";
 
 
+}); */
 
-});
+console.log("submit button", submitButton)
+const formHandler = async (e) => {
+    e.preventDefault();
+    try {
+
+        const body = {
+            profileName: document.getElementById("firstname").value,
+            profileAge: document.getElementById("age").value,
+            profileCity: document.getElementById("city").value,
+            profileDiscription: document.getElementById("message").value,
+            
+        }
+
+        const headers = new Headers();
+        headers.append("Content-Type", "application/json");
+        const res = await fetch("http://localhost:3000/api/newProfile", {method: 'POST', body: JSON.stringify(body),headers})
+        
+        const data = await res.json();
+
+        console.log("DATA FROM SERVER", data);
+        
+    } catch(err) {
+        console.error("Error sending the form data", err)
+    }
+
+}
+
+
+submitButton.addEventListener('click', formHandler)
+
 
--- server.js
+++ server.js
@@ -3,7 +3,76 @@ const app = express()
 const port = 3000
 const ejs = require('ejs')
 const mongoose  = require('mongoose')
+const bodyParser = require('body-parser');
 require('dotenv').config()
+
+
+
+//Database config
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://" + process.env.DB_NAME + ":" + process.env.DB_KEY + "@blocktech-aia3c.azure.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+let db = null;
+
+app.use(bodyParser.json())
+
+MongoClient.connect(uri, function (err, client) {
+    if (err) {
+      throw err
+    }
+    
+
+  
+    db = client.db("datingapp");
+
+})
+
+MongoClient.connect('connect', (err, connected) => {
+    console.log("Conection setted")
+})
+
+
+
+// find persons | index
+app.get("/index", async (req, res) => {
+    const profiles = await db.collection('profiles').find().toArray();
+    res.render("base/index.ejs", {
+        data, profiles
+    });
+   
+    collection.insertOne({naam: "test"})
+});
+
+app.get("/settings", (req, res) => {
+    res.render("base/settings.ejs", {
+        data, profile
+    });
+});
+
+
+app.post("/api/newProfile", async (req, res) => {
+
+    console.log("BODY DATA", req.body);
+    await db.collection('profiles').insertOne(req.body);
+    const documents = await db.collection('profiles').find().toArray();
+
+    console.log("DOCUMENTS", documents)
+    res.send({data: documents})
+})
+// sign up profile
+app.get("/newProfile", async (req, res) => {
+   
+   
+    res.render("base/newProfile.ejs", {
+        data, profile
+    });
+});
+
+
+
+function form (req, res) {
+    res.render('newProfile.ejs')
+}
 
 
 let data = {
@@ -12,59 +81,35 @@ let data = {
     name: "test"
 };
 
+
+
 let profile = {
     profileName: "Alexane Collins",
     profileAge: 20,
     profileCity: "Amsterdam",
-    profileDiscription: "somthing about the person somthingsomthing about the person somthing about about"
+    profileDiscription: "somthing about the person somthingsomthing about the person somthing about about",
+    
 };
+
+
+
 
 app.use(express.static('public'))
 app.set("view engine", "ejs")
-// app.set("views", "base")
 
 
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://" + process.env.DB_NAME + ":" + process.env.DB_KEY + "@blocktech-aia3c.azure.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-let collection = null;
-
-
-MongoClient.connect(uri, function (err, client) {
-    if (err) {
-      throw err
-    }
-  
-  
-    collection = client.db("datingapp").collection("profiles");
-
-})
- 
-
-app.get("/about", (req, res) => {
-    res.render("base/about.ejs", {
-        data, profile
-    });
-});
-
-
-app.get("/index", (req, res) => {
-    res.render("base/index.ejs", {
-        data, profile
-    });
-    // collection.insertOne({naam: "qq"})
-
-});
 
 
 
-// app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`listening on port ${port}`))
 
 
+// 404 page not found
 app.use(function (req, res) {
     res.send("404: Page not found", 404);
 });
  
+
+
+app.listen(port, () => console.log(`listening on port ${port}`))
+
